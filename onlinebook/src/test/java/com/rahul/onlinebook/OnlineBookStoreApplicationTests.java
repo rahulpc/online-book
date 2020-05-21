c19dcf92046e44aedd54bfc607d834b1c4f55b77
rahul <rahulkhilari0@gmail.com>
2020-05-21 11:52:53 +0530

    Initial commit. create spring boot project and configure the application properties.

---
+++ onlinebook/src/test/java/com/rahul/onlinebook/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.onlinebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
